81406b1d7545b339d7460502b2415b3a0dbd2c77
Ulises Moralez <umoralez@solvd.com>
2022-10-04 11:32:36 -0300

    Run the new api call

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { AuthToken } from './types/auth/AuthToken';
-import {getRefreshToken} from './apiMethods/apiCalls';
+import { authToken } from './types/auth/AuthToken';
+import {getRefreshToken, getTestRunStart} from './apiMethods/apiCalls';
+import { TestRunStart } from './types/testRun/TestRunStart';
 
-let authToken: AuthToken;
-getRefreshToken(authToken);
-console.log(authToken);
-
+let authToken: authToken;
+//getRefreshToken(authToken);
+//console.log(authToken);
+let testRunStart: TestRunStart = {
+  projectKey: 'ALPHA',
+  name: "TestNG run api",
+  startedAt: "2021-06-06T10:12:14.333+01:00",
+  status: "IN_PROGRESS",
+  framework: "testng"
+};
+getTestRunStart(testRunStart);
