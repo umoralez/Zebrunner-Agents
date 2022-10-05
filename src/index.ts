284bc4c3d20d3b43f74d39620fd9d3aa293011ba
Ulises Moralez <umoralez@solvd.com>
2022-10-05 17:06:46 -0300

    Refractor index.js - Create a main function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 import { authToken } from './types/auth/AuthToken';
-import {getRefreshToken, getTestRunStart} from './apiMethods/apiCalls';
-import { TestRunStart } from './types/testRun/TestRunStart';
+import {getRefreshToken, getTestRunStart, setTestExecutionStart} from './apiMethods/apiCalls';
+import { testRunStart } from './types/testRun/TestRunStart';
+import { testExecutionStart } from './types/testRun/TestExecutionStart';
 
-let authToken: authToken;
-//getRefreshToken(authToken);
-//console.log(authToken);
-let testRunStart: TestRunStart = {
+let testRunStart: testRunStart = {
   projectKey: 'ALPHA',
-  name: "TestNG run api",
-  startedAt: "2021-06-06T10:12:14.333+01:00",
+  name: "TypeScript Test ULI",
+  startedAt: "2022-10-04T10:12:14.333+01:00",
   status: "IN_PROGRESS",
-  framework: "testng"
-};
-getTestRunStart(testRunStart);
+  framework: "cypress"
+}
+
+let testExecutionStart: testExecutionStart = {
+  "name": "Test ULI",
+  "className": "com.test.MyTests",
+  "methodName": "uliTest()",
+  "startedAt": "2022-10-04T10:12:15.444+01:00"
+}
+
+//Promise.all verlo uwu
+async function main() {
+
+  //let refreshToken: authToken = await getRefreshToken();
+  //console.log(refreshToken.authToken);
+
+  let preparedTestData : testRunStart = await getTestRunStart(testRunStart)
+  console.log(preparedTestData);
+
+  let executionStart : testExecutionStart = await setTestExecutionStart(preparedTestData.id, testExecutionStart);
+  console.log(executionStart);
+  
+}
+
+main();
