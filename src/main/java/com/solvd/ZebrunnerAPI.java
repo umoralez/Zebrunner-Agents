b3baf0059e36ebd845e2d1f6ba5920f6b2f08481
Felipe Copello <felipecopello94@gmail.com>
2022-10-05 16:51:35 +0200

    added screenshot call to API

--- src/main/java/com/solvd/ZebrunnerAPI.java
+++ src/main/java/com/solvd/ZebrunnerAPI.java
@@ -2,7 +2,13 @@ package com.solvd;
 
 import static com.solvd.utils.FileUtils.removeInitialSpaces;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import com.google.gson.JsonObject;
 import com.solvd.domain.TestExcecutionFinishDTO;
@@ -13,6 +19,7 @@ import com.solvd.domain.TokenGenerationDTO;
 import com.solvd.utils.FileUtils;
 import com.solvd.utils.RequestUpdate;
 import com.solvd.utils.ResponseUtils;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
@@ -20,6 +27,7 @@ import com.squareup.okhttp.Response;
 public class ZebrunnerAPI extends BaseClass {
 
 	private final String endpoint = properties.getProperty("URL");
+	private String keyToken = "Authorization";
 
 	public void tokenGeneration() {
 		String token = FileUtils.propertyValue("access-token").get("access-token");
@@ -99,6 +107,37 @@ public class ZebrunnerAPI extends BaseClass {
 		} catch (IOException e) {
 			LOGGER.error(e.getMessage());
 		}
+	}
+
+	public void testScreenshotCollectionRequest(JsonObject endpointData, File screenshot) {
+		String projectId = FileUtils.readValueInProperties(idPath, "id") + "/tests/"
+				+ FileUtils.readValueInProperties(idPath, "test_id=") + "/screenshots";
+		String endpointScreenshotCollection = endpoint
+				.concat(RequestUpdate.addQueryParamsValue("ENP_EXECUTION", projectId));
+
+		BufferedImage bufferedImage;
+		MediaType MEDIA_TYPE_HTTP = MediaType.parse("image");
+
+		try {
+			bufferedImage = ImageIO.read(screenshot);
+
+			WritableRaster raster = bufferedImage.getRaster();
+			DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+
+			byte[] bodyImage = data.getData();
+
+			RequestBody body = RequestBody.create(MEDIA_TYPE_HTTP, bodyImage);
+
+			Request request = new Request.Builder().url(endpointScreenshotCollection).put(body)
+					.addHeader(keyToken, FileUtils.readValueInProperties(tokenPath, "Authorization=Bearer ")).build();
+
+			Response response = client.newCall(request).execute();
+			response.body().close();
+
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage());
+		}
+
 	}
 
 	public void testExecutionFinishRequest(JsonObject endpointData) {
